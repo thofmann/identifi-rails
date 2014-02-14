9d0f49967720ed0850f1f48749c7ddb2fb8358ad
Martti Malmi <martti.malmi@aalto.fi>
2014-02-14 15:49:25 +0200

    getpacketsafter -> getlatestpackets

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,6 @@ require 'identifi-rpc'
 class SearchController < ApplicationController
   def index
     h = IdentifiRPC.new(IdentifiRails::Application.config.identifiHost)
-    @latest = h.getpacketsafter( "0", "20" )
+    @latest = h.getlatestpackets( "10" )
   end
 end
