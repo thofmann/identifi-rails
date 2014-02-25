320fb70e68be5d0be77821693103c3ea25ae1ea0
Martti Malmi <martti.malmi@aalto.fi>
2014-02-26 00:21:05 +0200

    count only reviews in the stats box

--- app/controllers/identifier_controller.rb
+++ app/controllers/identifier_controller.rb
@@ -12,15 +12,17 @@ class IdentifierController < ApplicationController
     @authoredNegative = 0
 
     @authored.each do |m|
-      signedData = m["data"]["signedData"] 
-      rating = signedData["rating"]
-      neutralRating = (signedData["minRating"] + signedData["maxRating"]) / 2
-      if rating > neutralRating
-        @authoredPositive += 1
-      elsif rating < neutralRating
-        @authoredNegative += 1
-      else
-      	@authoredNeutral += 1
+      signedData = m["data"]["signedData"]
+      if signedData["type"] == "review"
+        rating = signedData["rating"]
+        neutralRating = (signedData["minRating"] + signedData["maxRating"]) / 2
+        if rating > neutralRating
+          @authoredPositive += 1
+        elsif rating < neutralRating
+          @authoredNegative += 1
+        else
+        	@authoredNeutral += 1
+        end
       end
       signedData["author"].each do |a|
         @mentionedWith.push(a) unless a.second == params[:value]
@@ -32,15 +34,17 @@ class IdentifierController < ApplicationController
     @receivedNegative = 0
 
     @received.each do |m|
-      signedData = m["data"]["signedData"] 
-      rating = signedData["rating"]
-      neutralRating = (signedData["minRating"] + signedData["maxRating"]) / 2
-      if rating > neutralRating
-        @receivedPositive += 1
-      elsif rating < neutralRating
-        @receivedNegative += 1
-      else
-      	@receivedNeutral += 1
+      signedData = m["data"]["signedData"]
+      if signedData["type"] == "review"
+        rating = signedData["rating"]
+        neutralRating = (signedData["minRating"] + signedData["maxRating"]) / 2
+        if rating > neutralRating
+          @receivedPositive += 1
+        elsif rating < neutralRating
+          @receivedNegative += 1
+        else
+        	@receivedNeutral += 1
+        end
       end
       signedData["recipient"].each do |a|
         @mentionedWith.push(a) unless a.second == params[:value]
