8a7f88c1e70649278932a88a6885a4d1a645967b
Martti Malmi <martti.malmi@aalto.fi>
2014-03-10 14:43:29 +0200

    hide charts & graph for now, move omniauth keys to config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '73899182844579', 'fdf9dcf8bfb9935886cf5c947e9ab30d'
+  provider :facebook, IdentifiRails::Application.config.fbID, IdentifiRails::Application.config.fbKey
+  provider :twitter, IdentifiRails::Application.config.twitterKey, IdentifiRails::Application.config.twitterSecret
 end
