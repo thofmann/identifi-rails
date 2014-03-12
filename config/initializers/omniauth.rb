33a10fec4ecf76cd9425a51f93c49b969fa78d6a
Martti Malmi <martti.malmi@aalto.fi>
2014-03-12 22:17:56 +0200

    auth/developer, logged in menu

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
   provider :facebook, IdentifiRails::Application.config.fbID, IdentifiRails::Application.config.fbKey
   provider :twitter, IdentifiRails::Application.config.twitterKey, IdentifiRails::Application.config.twitterSecret
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ IdentifiRails::Application.routes.draw do
 
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+  match 'logout', to: 'sessions#destroy', as: 'logout', via: [:get, :post]
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
