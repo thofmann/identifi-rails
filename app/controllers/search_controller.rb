23c4f2880d9ad348a564355070d337262f82e0f7
Martti Malmi <martti.malmi@aalto.fi>
2014-08-26 04:41:43 +0300

    viewpointed search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
   def index
     h = IdentifiRPC.new(IdentifiRails::Application.config.identifiHost)
-    @results = h.search(params[:query]);
+    @nodeID = IdentifiRails::Application.config.nodeID
+    if current_user
+      @results = h.search(params[:query], "", "5", "0", current_user.type, current_user.value);
+    else
+      @results = h.search(params[:query], "", "5", "0", @nodeID[0], @nodeID[1]);
+    end
     respond_to do |format|
       format.html
       format.json { render :json => @results }
