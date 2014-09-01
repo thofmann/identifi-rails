df6ab6f011a63d88c4d0730efac7e5d1a8895d57
Martti Malmi <sirius@iki.fi>
2014-09-01 15:37:23 -0400

    Use jquery-ui-rails gem, fix missing assets and search style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui.min
+//= require jquery-ui/autocomplete
 //= require jquery.infinitescroll.min
 //= require spin.min
 //= require_tree .
