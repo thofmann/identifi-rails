528589f6893a9f46ca2e012cbfaf05c0a31bc6a7
Martti Malmi <sirius@iki.fi>
2014-10-15 10:53:30 -0400

    Identity search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,17 +8,33 @@ class SearchController < ApplicationController
     offset = (params[:page].to_i * RESULT_COUNT) or 0
     @results = []
     if current_user
-      @results = h.search(params[:query] || "", "", RESULT_COUNT.to_s, offset.to_s, current_user.type, current_user.value);
+      @rawResults = h.search(params[:query] || "", "", RESULT_COUNT.to_s, offset.to_s, current_user.type, current_user.value);
     else
-      @results = h.search(params[:query] || "", "", RESULT_COUNT.to_s, offset.to_s, @nodeID[0], @nodeID[1]);
+      @rawResults = h.search(params[:query] || "", "", RESULT_COUNT.to_s, offset.to_s, @nodeID[0], @nodeID[1]);
     end
-    @results.each do |r|
-      if r["email"].empty?
-        gravatar = "#{r["type"]}:#{r["value"]}"
-      else
-        gravatar = r["email"]
+    @rawResults.each do |r|
+      result = {"type" => "", "value" => "", "email" => "", "name" => ""}
+      r.each do |id|
+          case id[0]
+          when "name" || "nickname"
+            result["name"] = id[1]
+          when "email"
+            result["email"] = id[1]
+          when "url"
+            result["facebook"] = id[1] if id[1].include? "facebook.com/"
+            result["twitter"] = id[1] if id[1].include? "twitter.com/"
+            result["google_plus"] = id[1] if id[1].include? "plus.google.com/"
+          end
       end
-      r["gravatarHash"] = getGravatarHash(gravatar)
+      result["type"] = r[0][0]
+      result["value"] = r[0][1]
+      if result["email"].empty?
+        gravatar = "#{result["type"]}:#{result["value"]}"
+      else
+        gravatar = result["email"]
+      end
+      result["gravatarHash"] = getGravatarHash(gravatar)
+      @results << result
     end
     respond_to do |format|
       format.html
